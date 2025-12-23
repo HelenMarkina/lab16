17de20c1fe67755ebfe9350700a2c0612f2d3dc5
HelenMarkina <elenamar05@mail.ru>
2025-12-23 18:25:54 +0300

    1

--- lab16.c
+++ lab16.c
@@ -4,6 +4,7 @@
 #include <locale.h>
 #include <math.h>
 #include <time.h>
+#include "array.h"
 
 double* full_elements(double* ptr_array, int size);
 int put_elements(double* ptr_array, int size);
@@ -88,37 +89,6 @@ int main() {
     return 0;
 }
 
-double* full_elements(double* ptr_array, int size) {
-    for (int i = 0; i < size; i++) {
-        double x = 1.3 + i * 0.1;
-        ptr_array[i] = 5 * pow(x, 3) + 2 * pow(x, 2) - 15 * x - 6;
-    }
-    return ptr_array;
-}
-
-int put_elements(double* ptr_array, int size) {
-    printf("\na[%d] =", size);
-
-    for (int i = 0; i < size; i++)
-    {
-        printf(" %.2f", ptr_array[i]);
-    }
-}
-
-double* calc_elements(double* ptr_array, int size) {
-    for (int i = 0; i < size; i++)
-    {
-        ptr_array[i] = ceilf(ptr_array[i]);
-    }
-    return ptr_array;
-}
-
-double* fill_random(double* ptr_array, int size) {
-    for (int i = 0; i < size; i++) {
-        ptr_array[i] = (rand() % 201 - 100) / 100.0;
-    }
-    return ptr_array;
-}
 
 int delete_k(double* ptr_arr, int size, double element) {
     int new_size = 0;
